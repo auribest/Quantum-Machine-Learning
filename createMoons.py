0263547893a3456bf6f366831e3e9652daf6647e
Andres Uribe Stengel <andres.uribe_stengel@hotmail.com>
2021-06-14 16:26:44 +0200

    Extend half moon dataset creator script to create JSON dataset files.

--- createMoons.py
+++ createMoons.py
@@ -1,13 +1,82 @@
-import matplotlib.pyplot as plt
-import sklearn.datasets
-from sklearn.datasets import make_moons
+import json
+from json import JSONEncoder
 import numpy as np
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.preprocessing import LabelEncoder
+from sklearn.datasets import make_moons
+
 
 # n_samples = amount of samples, shuffle = shuffle data, noise = random factor of position, random_state = seed
 # if shuffle is False the data contains all values of class 0 first and then all values of class 1 ...
 numbersamples = 1000
-data = make_moons(n_samples=numbersamples, noise=0.275, random_state=1)
+seed = 1
+data = make_moons(n_samples=numbersamples, noise=0.275, random_state=seed)
 print("moons length: %d entries: %d" % (len(data), len(data[0])))
 
-np.save("archives/HalfMoon/halfmoon_%d_data.npy"%numbersamples, data[0])
-np.save("archives/HalfMoon/halfmoon_%d_label.npy"%numbersamples, data[1])
+# Get samples and labels from data
+x_data = data[0]
+y_data = data[1]
+
+# Create numpy arrays
+data = np.array(x_data)
+labels = np.array(y_data)
+
+
+# Initialize the split array tuples
+train_data = [], []
+val_data = [], []
+test_data = [], []
+
+# Initialize shuffle-split (for the train and validation data)
+sss1 = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=seed)
+
+# Shuffle and split into train data and validation data
+for train_index, val_index in sss1.split(data, labels):
+    x_train = data[train_index]
+    y_train = labels[train_index]
+    x_val = data[val_index]
+    y_val = labels[val_index]
+
+    train_data = x_train, y_train
+    val_data = x_val, y_val
+
+# Set and transform labels
+train_encoder = LabelEncoder()
+train_labels = train_data[1]
+train_encoder.fit(train_labels)
+train_labels = train_encoder.transform(train_labels)
+
+val_encoder = LabelEncoder()
+val_labels = val_data[1]
+val_encoder.fit(val_labels)
+val_labels = val_encoder.transform(val_labels)
+
+# Unpack the train and val samples from the tuple
+train_data = train_data[0]
+val_data = val_data[0]
+
+print(train_data.shape)
+print(val_data.shape)
+
+class NumpyArrayEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return JSONEncoder.default(self, obj)
+
+# Serialization
+jsonData = {
+                "TrainingData":
+                    {
+                        "Features": train_data.tolist(),
+                        "Labels": train_labels.tolist()
+                    },
+                "ValidationData":
+                    {
+                        "Features": val_data.tolist(),
+                        "Labels": val_labels.tolist()
+                    }
+            }
+
+with open('archives/HalfMoon/data_' + str(numbersamples) + '.json', 'w+') as outfile:
+    json.dump(jsonData, outfile)
